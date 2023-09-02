36b7ec66c8a88b67309c95551294cab30d55834b
marienrique <mariela.enrique@gmail.com>
2023-09-01 21:00:46 -0300

    Adding AtomicInteger in FilterCustom

--- gateway/src/main/java/com/dh/gateway/filter/FilterCustom.java
+++ gateway/src/main/java/com/dh/gateway/filter/FilterCustom.java
@@ -5,12 +5,15 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 @Component
 public class FilterCustom extends AbstractGatewayFilterFactory<FilterCustom.Config> {
 
     private static Logger log = Logger.getLogger(FilterCustom.class.getName());
+
+    private static AtomicInteger COUNT_CALL_GATEWAY = new AtomicInteger(0);
 
     public FilterCustom() {
         super(Config.class);
@@ -23,6 +26,8 @@ public class FilterCustom extends AbstractGatewayFilterFactory<FilterCustom.Conf
             log.info("Path requested: " + exchange.getRequest().getPath());
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Cantidad de llamadas al Gateway: " + COUNT_CALL_GATEWAY.incrementAndGet());
+
                 log.info("Time response: " + Calendar.getInstance().getTime());
             }));
         };
